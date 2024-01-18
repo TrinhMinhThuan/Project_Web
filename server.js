bd3f6c8f3721b217048592f8eba04e6d2b4a53e0
21120594 <21120594@student.hcmus.edu.vn>
2024-01-18 22:10:36 +0700

    Add server Admin

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { create } = require("express-handlebars");
+const app = express();
+
+
+const hbs = create({
+  extname: '.hbs'
+});
+
+app.engine('hbs', hbs.engine)
+
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.get("/", (req,res,next) =>{
+  res.render("searchCategories", {
+    title: "Quản lý danh mục",
+  });
+});
+
+app.get("/searchCategories", (req,res,next) =>{
+  res.render("searchCategories", {
+    title: "Quản lý danh mục",
+  });
+});
+
+app.get("/addCategories", (req,res,next) =>{
+  res.render("addCategories", {
+    title: "Quản lý danh mục",
+  });
+});
+
+app.get("/addBook", (req,res,next) =>{
+  res.render("addBook", {
+    title: "Thêm danh mục",
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
