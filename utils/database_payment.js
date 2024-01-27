70676f3bd14f380332dfcfb085150b75dbde3c4c
21120567 <21120567@student.hcmus.edu.vn>
2024-01-27 13:06:49 +0700

    add database info

---
+++ utils/database_payment.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_PAYMENT_NAME,
+  server: process.env.DB_SERVER,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+module.exports = config;
